f5dd63d5dfcb4ecaf836c84510ee95b86ce4e872
Ben Leeming <benleeming1992@gmail.com>
2025-10-19 22:37:42 +0100

    Begin investigating prophet basic forecast

--- modelling.py
+++ modelling.py
@@ -1,7 +1,23 @@
-import sqlite3
+import datetime
 
 import pandas as pd
+from prophet import Prophet
 
 raw_data = pd.read_csv("data/output.csv")
 
-raw_data.head()
+tidy_data = raw_data.rename(columns = {'date':'ds', 'price':'y'})[['ds', 'y']]
+
+model = Prophet()
+
+model.fit(tidy_data)
+
+future_period = model.make_future_dataframe(periods=12 * 7)
+
+forecast = model.predict(future_period)
+
+print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
+
+
+fig1 = model.plot(forecast)
+
+fig1.savefig("data/forecast.png")
